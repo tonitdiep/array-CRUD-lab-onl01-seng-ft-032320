8bbb499d23af820214d70d16b69c0f731272f2b3
Toni  Diep <adiep123@gmail.com>
2020-03-26 00:09:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-    push.array << "stew" 
+    ["tea", "coffee", "wine", "broth"] << "stew"
 end
 
 def add_element_to_start_of_array(array, element)
