878889538427675984e48b74b198510c0284158d
Toni  Diep <adiep123@gmail.com>
2020-03-26 01:36:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-    ["tea", "coffee", "wine", "broth", "stew"]
+    ["tea", "coffee", "wine", "broth"]
 end
 
 def add_element_to_end_of_array(array, element)
